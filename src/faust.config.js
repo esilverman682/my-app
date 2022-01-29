453020e610f4ddbc9abf351662107ee611343340
W Rivera <wrivera@Ws-iMac.fios-router.home>
2022-01-29 09:58:46 -0500

    new commit

---
+++ src/faust.config.js
@@ -0,0 +1,22 @@
+import { config as coreConfig } from '@faustjs/core';
+import { config as nextConfig } from '@faustjs/next';
+
+
+
+if (!process.env.NEXT_PUBLIC_WORDPRESS_URL) {
+  console.error(
+    'You must provide a NEXT_PUBLIC_WORDPRESS_URL environment variable, did you forget to load your .env.local file?',
+  );
+}
+
+/**
+ * @type {import("@faustjs/core").Config}
+ */
+export default coreConfig({
+  wpUrl: process.env.NEXT_PUBLIC_WORDPRESS_URL,
+  apiClientSecret: process.env.FAUSTWP_SECRET_KEY,
+});
+
+nextConfig({
+  revalidate: 1, // 60 seconds
+});
